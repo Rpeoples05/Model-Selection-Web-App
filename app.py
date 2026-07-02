c3888bfbb34067d1def5f6defeb715ffcf9d7659
Rpeoples05 <ryan_peoples@outlook.com>
2026-07-02 20:28:41 +0100

    Initial Commit

---
+++ app.py
@@ -0,0 +1,17 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.svm import SVC
+from sklearn.linear_model import LogisticRegression
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.preprocessing import LabelEncoder
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import ConfusionMatrixDisplay, RocCurveDisplay, PrecisionRecallDisplay
+from sklearn.metrics import precision_score, recall_score
+
+def main():
+    
+
+if __name__ == '__main__':
+    main()
