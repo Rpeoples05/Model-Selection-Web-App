3f3239a5ce69c4c5be1f8d39550a5e4765f8c14c
Rpeoples05 <ryan_peoples@outlook.com>
2026-07-02 22:01:37 +0100

    Add Load and Preprocess functions

--- app.py
+++ app.py
@@ -9,9 +9,18 @@ from sklearn.preprocessing import LabelEncoder
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import ConfusionMatrixDisplay, RocCurveDisplay, PrecisionRecallDisplay
 from sklearn.metrics import precision_score, recall_score
+from model import preprocessing
 
 def main():
-    
+    st.title("Model Selection Web App")
+    st.markdown("Is your wine good quality?")
+
+    @st.cache_data(persist = True)
+    def load_process(dataset):
+        data = preprocessing.load_data(dataset)
+        preprocessing.preprocess(dataset, data)
+
+    df = load_process("Wine Quality Classifier")
 
 if __name__ == '__main__':
     main()
---
+++ model/preprocessing.py
@@ -0,0 +1,41 @@
+import pandas as pd
+import numpy as np
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler, LabelEncoder
+
+def load_data(dataset):
+    if dataset == "Wine Quality Classifier":
+        white_wine = pd.read_csv("data/winequality-white.csv", sep = ";")
+        white_wine["type"] = "White"
+        red_wine = pd.read_csv("data/winequality-red.csv", sep = ";")
+        red_wine["type"] = "Red"
+        df = pd.concat([white_wine, red_wine])
+    return df
+
+def preprocess(dataset, df):
+    print(df.columns)
+    if dataset == "Wine Quality Classifier":
+        df["category"] = "Okay"
+
+        df.loc[df['quality'] < 4, "category"] = "Bad"
+        df.loc[df['quality'] > 7, "category"] = "Good"
+
+        df['category'] = df['category'].astype('category')
+        df['type'] = df['type'].astype('category')
+        df = df.drop(columns = ['quality'])
+
+    categorical_cols = df.select_dtypes(include=['category']).columns.tolist()
+    numerical_cols = df.select_dtypes(include=['float64']).columns.tolist()
+
+    scaler = StandardScaler()
+    labelencoder = LabelEncoder()
+
+    scaled_data = scaler.fit_transform(df[numerical_cols])
+    df_scaled = pd.DataFrame(scaled_data, columns = numerical_cols, index = df.index)
+
+    for col in categorical_cols:
+        df[col] = labelencoder.fit_transform(df[col])
+
+    df = pd.concat([df_scaled, df[categorical_cols]], axis=1)
+
+    return df
